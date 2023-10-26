93cf30a4c8610c096a0e690e6b3c6ec20f025630
Alex Szendefy <szendefy.alex@petrik.hu>
2023-10-26 11:41:16 +0200

    Kezdeti beállítások, Szuperhős és Milliárdos interfész, valamint Bosszuálló class

---
+++ SzuperhosProjekt/Bosszuallo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzuperhosProjekt
+{
+    public abstract class Bosszuallo : ISzuperhos
+    {
+        private double szuperero;
+        private bool vaEGyengesege;
+
+        protected Bosszuallo(double szuperero, bool vaEGyengesege)
+        {
+            this.szuperero = szuperero;
+            this.vaEGyengesege = vaEGyengesege;
+        }
+
+        public double Szuperero { get => szuperero; set => szuperero = value; }
+        public bool VaEGyengesege { get => vaEGyengesege; set => vaEGyengesege = value; }
+
+        public bool LegyoziE(ISzuperhos szuperhos)
+        {
+            if (szuperhos.GetType() == typeof(Bosszuallo))
+            {
+                if (((Bosszuallo)szuperhos).VaEGyengesege)
+                {
+                    if (this.MekkoraAzEreje()>szuperhos.MekkoraAzEreje())
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else if (szuperhos.GetType() == typeof(Batman))
+            {
+                if (this.MekkoraAzEreje() >= szuperhos.MekkoraAzEreje()*2)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                throw new Exception("Nincs ilyen szuperhős");
+            }
+        }
+
+        public double MekkoraAzEreje()
+        {
+            return this.szuperero;
+        }
+        public abstract bool MegmentiAVilagot();
+        public override string ToString()
+        {
+            return $"Szupererő: {this.MekkoraAzEreje()}; { (vaEGyengesege? "van gyengesége" : "nincs gyengesége")}";
+        }
+    }
+}
