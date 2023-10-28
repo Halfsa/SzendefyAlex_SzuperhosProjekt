817ee6f2ce48ad932ecae8fd2176bd820ddb83de
Alex Szendefy <szendefy.alex@petrik.hu>
2023-10-28 11:58:47 +0200

    Vasember osztály

--- SzuperhosProjekt/Vasember.cs
+++ SzuperhosProjekt/Vasember.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace SzuperhosProjekt
 {
-    internal class Vasember
+    public class Vasember : Bosszuallo , Milliardos
     {
+        public Vasember(double szuperero, bool vaEGyengesege) : base(szuperero, vaEGyengesege)
+        {
+            szuperero = 150;
+            vaEGyengesege = true;
+        }
+
+        public void KutyutKeszit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool MegmentiAVilagot()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
